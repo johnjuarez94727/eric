42b153dd3c903ba5bcd089d264159263b8673a84
rubenponce9161-max <rubenponce9161@gmail.com>
2026-09-17 07:52:17 -0400

    add resume

--- src/features/Footer.tsx
+++ src/features/Footer.tsx
@@ -37,6 +37,22 @@ const links = [
     ),
     label: "LinkedIn",
   },
+  {
+    href: "https://drive.google.com/file/d/13PYRtesId0ELT_pgzVqQBXxoToWzCezk/view",
+    icon: (
+      <svg
+        aria-hidden="true"
+        fill="currentColor"
+        height="24"
+        viewBox="0 0 24 24"
+        width="24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M14 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11zM8 15h8v2H8v-2zm0-4h8v2H8v-2zm0-4h5v2H8V7z" />
+      </svg>
+    ),
+    label: "Resume",
+  },
 ];
 
 export const Footer = () => {
